807d213c0c26f5bdb74db3c62a619bc09cfb74ef
EkaterinaShulga <shulga23@gmail.com>
2022-07-20 13:52:18 +0300

    домашнее задание по теме 2.14 Основы алгоритмов, часть 1 - выполнила

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,102 @@
 package org.example;
 
+import org.example.interfaces.StringList;
+
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
+        MyArrayList<String> myArrayList = new MyArrayList<String>(10);
+        System.out.println("add---------------------------------------------------");
+        System.out.println(myArrayList.add("15"));
+        System.out.println(myArrayList);
+        System.out.println();
+
+        System.out.println("add(index)---------------------------------------------------");
+        myArrayList.add(1, "20");
+        myArrayList.add(2, "30");
+        myArrayList.add(3, "40");
+        myArrayList.add(4, "50");
+        myArrayList.add(5, "60");
+        myArrayList.add(6, "70");
+        myArrayList.add(7, "80");
+        myArrayList.add(8, "80");
+        myArrayList.add(9, "90");
+        System.out.println(myArrayList);
+        System.out.println();
+
+
+        System.out.println("set---------------------------------------------------");
+        System.out.println(myArrayList.set(0, "10"));
+        System.out.println(myArrayList);
+        System.out.println();
+
+
+        System.out.println("remove(item)---------------------------------------------------");
+        System.out.println(myArrayList.remove("80"));
+        System.out.println(myArrayList);
+        System.out.println();
+
+        System.out.println("remove(index)---------------------------------------------------");
+        System.out.println(myArrayList.remove(3));
+        System.out.println(myArrayList);
+        System.out.println(myArrayList.remove(5));
+        System.out.println(myArrayList);
+        System.out.println();
+
+
+        System.out.println("contains---------------------------------------------------");
+        System.out.println(myArrayList.contains("90"));
+        System.out.println();
+
+        System.out.println("indexOf---------------------------------------------------");
+        System.out.println(myArrayList.indexOf("90"));
+        System.out.println();
+
+        System.out.println("lastIndexOf---------------------------------------------------");
+        System.out.println(myArrayList.lastIndexOf("90"));
+        System.out.println();
+
+        System.out.println("get---------------------------------------------------");
+        System.out.println(myArrayList.get(1));
+        System.out.println();
+
+
+        System.out.println("equals---------------------------------------------------");
+        StringList otherList = new MyArrayList<String>() {
+        };
+        otherList.add(0, "10");
+        otherList.add(1, "20");
+        otherList.add(2, "30");
+        otherList.add(3, "50");
+        otherList.add(4, "60");
+        otherList.add(5, "80");
+
+
+        System.out.println(myArrayList);
+        System.out.println(otherList);
+        System.out.println(myArrayList.equals(otherList));
+        System.out.println();
+
+
+        System.out.println("size---------------------------------------------------");
+        System.out.println(myArrayList.size());
+        System.out.println();
+
+        System.out.println("isEmpty---------------------------------------------------");
+        System.out.println(myArrayList.isEmpty());
+        System.out.println();
+
+        System.out.println();
+        System.out.println("clear---------------------------------------------------");
+        myArrayList.clear();
+        System.out.println(myArrayList);
+        System.out.println();
+
+        System.out.println("myArrayList.toArray---------------------------------------------------");
+        System.out.println(Arrays.toString(myArrayList.toArray()));
+
 
     }
 }
---
+++ src/main/java/org/example/MyArrayList.java
@@ -0,0 +1,245 @@
+package org.example;
+
+import org.example.exceptions.MyArrayListIllegalArgumentException;
+import org.example.exceptions.MyArrayListIndexOutException;
+import org.example.interfaces.StringList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class MyArrayList<T> implements StringList {
+
+    private final String[] list;
+
+    private int capacity;
+    private final int DEFAULT_CAPACITY = 10;
+
+    public MyArrayList(int capacity) {
+        if (capacity <= 0) {
+            throw new MyArrayListIllegalArgumentException("Размер массива <= 0");
+        } else {
+            list = new String[capacity];
+        }
+    }
+
+    public MyArrayList() {
+        list = new String[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    public String add(String item) {
+        if (Objects.isNull(item)) {
+            throw new MyArrayListIllegalArgumentException("Передаваемое значение null");
+        }
+        if (capacity >= list.length) {
+            throw new MyArrayListIndexOutException("Все ячейки массива заполнены");
+        }
+        return list[capacity++] = item;
+    }
+
+    @Override
+    public String add(int index, String item) {
+        if (index > capacity) {
+            throw new MyArrayListIllegalArgumentException("Индекс: " + index + ", " + "количество элементов в списке: " + capacity);
+        }
+        if (index < 0) {
+            throw new MyArrayListIllegalArgumentException("Индекc должен быть больше 0");
+        }
+        if (list[index] != null) {
+            throw new MyArrayListIllegalArgumentException("ячейка уже занята");
+        } else if (item == null) {
+            throw new MyArrayListIllegalArgumentException("переданное значение = null");
+        }
+        for (int i = capacity; i > index; i--) {
+            list[i] = list[i - 1];
+        }
+        list[index] = item;
+        capacity++;
+        return item;
+    }
+
+    @Override
+    public String set(int index, String item) {
+        if (Objects.isNull(item)) {
+            throw new MyArrayListIllegalArgumentException("Передаваемое в список значение не должно быть null");
+        }
+        if (index >= capacity) {
+            throw new MyArrayListIllegalArgumentException("Индекс: " + index + ", " + "количество элементов в списке: " + capacity);
+        }
+        if (index < 0) {
+            throw new MyArrayListIllegalArgumentException("Индекc должен быть больше 0");
+        } else {
+            list[index] = item;
+        }
+        return item;
+    }
+
+    @Override
+    public String remove(String item) {
+        if (Objects.isNull(item)) {
+            throw new MyArrayListIllegalArgumentException("Передаваемое в список значение не должно быть null");
+        }
+        int indexToDelete = -1;
+        for (int i = 0; i < capacity; i++) {
+            if (item.equals(list[i])) {
+                indexToDelete = i;
+                break;
+            }
+        }
+        if (indexToDelete == -1) {
+            throw new MyArrayListIllegalArgumentException("Элемент не найден");
+        }
+
+        return remove(indexToDelete);
+    }
+
+
+    @Override
+    public String remove(int index) {
+        if (index > capacity) {
+            throw new MyArrayListIllegalArgumentException("Индекс: " + index + ", " + "количество элементов в списке: " + capacity);
+        }
+        if (index < 0) {
+            throw new MyArrayListIllegalArgumentException("Индекc должен быть больше 0");
+        }
+        String result = "";
+        String[] newList = new String[list.length];
+        if (index <= list.length) {
+            if (list[index] != null) {
+                result = list[index];
+                list[index] = null;
+                capacity--;
+            } else {
+                throw new MyArrayListIllegalArgumentException("ячейка не содержит данных");
+            }
+        }
+        for (int j = 0; j < list.length - 1; j++) {
+            if (j == index && j <= list.length - 1) {
+                list[j] = list[index + 1];
+                list[index + 1] = null;
+                index++;
+            }
+        }
+        return "удалено значение " + result;
+    }
+
+
+    @Override
+    public boolean contains(String item) {
+        if (Objects.isNull(item)) {
+            throw new MyArrayListIllegalArgumentException("Передаваемое в список значение не должно быть null");
+        }
+        for (int i = 0; i < capacity; i++) {
+            if (item.equals(list[i])) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    @Override
+    public int indexOf(String item) {
+        if (Objects.isNull(item)) {
+            throw new MyArrayListIllegalArgumentException("Передаваемое в список значение не должно быть null");
+        }
+        int result = -1;
+        for (int i = 0; i < list.length; i++) {
+            if (item.equals(list[i])) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        if (Objects.isNull(item)) {
+            throw new MyArrayListIllegalArgumentException("Передаваемое в список значение не должно быть null");
+        }
+        int result = -1;
+        for (int i = list.length - 1; i >= 0; i--) {
+            if (item.equals(list[i])) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String get(int index) {
+        String result = "";
+        if (index > capacity) {
+            throw new MyArrayListIllegalArgumentException("Индекс: " + index + ", " + "количество элементов в списке: " + capacity);
+        }
+        if (index < 0) {
+            throw new MyArrayListIllegalArgumentException("Индекc должен быть больше 0");
+        }
+        return list[index];
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        if (Objects.isNull(otherList)) {
+            throw new MyArrayListIllegalArgumentException("Передаваемый список не должен быть null");
+        }
+        if (size() != otherList.size()) {
+            return false;
+        }
+        for (int i = 0; i < list.length; i++) {
+            if (!list[i].equals(otherList.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    @Override
+    public int size() {
+        return capacity;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        for (int i = 0; i < capacity; i++) {
+            if (list[i] != null) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < capacity; i++) {
+            list[i] = null;
+        }
+    }
+
+    @Override
+    public String[] toArray() {
+        String[] toArray = new String[capacity];
+        for (int i = 0; i < capacity; i++) {
+            toArray[i] = list[i];
+        }
+        System.out.println("создан новый массив" + Arrays.toString(toArray));
+        return toArray;
+    }
+
+
+    @Override
+    public String toString() {
+        return "MyArrayList{" +
+                "list=" + Arrays.toString(list) +
+                ", size=" + capacity +
+                ", DEFAULT_CAPACITY=" + DEFAULT_CAPACITY +
+                '}';
+    }
+
+
+}
